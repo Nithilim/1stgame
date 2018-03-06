b0128cf174489be7b9c0bdc7a8344c684f47a941
Nith-PC\Nith <bebrutvartas@gmail.com>
2018-03-06 21:38:34 +0200

    Creating Hero and Enemy structures for game.

---
+++ ConsoleApp5/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Enemy
+    {
+        private int Id;
+        private int XCord;
+        private int YCord;
+        private string EnemyName;
+
+        public Enemy(string EnemyName, int id, int x, int y)
+        {
+            this.EnemyName = EnemyName;
+            Id = id;
+            XCord = x;
+            YCord = y;
+        }
+
+        public void MoveRight()
+        {
+            XCord = XCord + 1;
+        }
+        public void MoveLeft()
+        {
+            XCord = XCord - 1;
+        }
+        public void MoveUp()
+        {
+            YCord = YCord - 1;
+        }
+        public void MoveDown()
+        {
+            YCord = YCord + 1;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Current {EnemyName} {Id} coordinates X: {XCord}; Y: {YCord}");
+        }
+    }
+}
---
+++ ConsoleApp5/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Hero
+    {
+        private string HeroName;
+        private int XCord;
+        private int YCord;
+
+        public Hero(string HeroName, int x, int y)
+        {
+            this.HeroName = HeroName;
+            XCord = x;
+            YCord = y;
+        }
+
+        public void MoveRight()
+        {
+            XCord = XCord + 1;
+        }
+        public void MoveLeft()
+        {
+            XCord = XCord - 1;
+        }
+        public void MoveUp()
+        {
+            YCord = YCord - 1;
+        }
+        public void MoveDown()
+        {
+            YCord = YCord + 1;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Current {HeroName} coordinates X: {XCord}; Y: {YCord}");
+        }
+    }
+}
